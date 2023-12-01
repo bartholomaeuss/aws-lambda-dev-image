f6d39bd5fce3ca4297f2b7977e02e4ab0fb68c8b
bartholomaeuss <bartholomaeus.s@googlemail.com>
2023-12-01 17:59:12 +0100

    Update main script
    Update README
    
    Update Dockerfile
    
    Add README
    
    Add main shell script
    
    Add hello world
    
    Add gitignore
    
    Update Dockerfile
    
    Add Dockerfile

---
+++ README.md
@@ -0,0 +1,28 @@
+# AWS LAMBDA DEV IMAGE
+
+### Prerequisite
+
+As of now the code is tested only on windows local machines.
+
+```bash
+./hello_world.sh
+```
+
+### Windows
+
+```bash
+./provide_container.sh
+```
+
+### More
+
+```
+ssh -p 2223 -i ~/.ssh/<key> <user>@localhost
+```
+
+See the official
+[docker build](https://docs.docker.com/engine/reference/commandline/build/)
+documentation.
+See also the official
+[docker run](https://docs.docker.com/engine/reference/commandline/run/)
+documentation.
---
+++ provide_container.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+clean_uninstall=false
+install=false
+
+show_help(){
+    echo "Deploy basic ssh config to $HOME/.ssh/config."
+    echo "usage: $0 [-d] [-i] [-r] [-t] [-u] [-h]"
+    echo "  -d  dockerfile name"
+    echo "  -i  image name"
+    echo "  -r  remote host name"
+    echo "  -t  image tag name"
+    echo "  -u  remote host user name"
+    echo "  -h  show help"
+    exit 0
+}
+
+provide_container(){
+    scp "${dockerfile}" "${user}@${remote}":"~/${dockerfile}"
+    ssh -l "${user}" "${remote}" "docker kill \$(docker ps -q --filter ancestor=${image}:${tag})"
+    ssh -l "${user}" "${remote}" "docker build -t ${image}:${tag} -f ./${dockerfile} ."
+    ssh -l "${user}" "${remote}" "docker run -d -v /home/${user}/pycharm_project:/var/task -p 2222:22 ${image}:${tag}"
+}
+
+while getopts ":d:i:r:t:u:h" opt; do
+  case $opt in
+    d)
+      dockerfile="$OPTARG"
+      ;;
+    i)
+      image="$OPTARG"
+      ;;
+    r)
+      remote="$OPTARG"
+      ;;
+    t)
+      tag="$OPTARG"
+      ;;
+    u)
+      user="$OPTARG"
+      ;;
+    h)
+      show_help
+      ;;
+    \?)
+      echo "unknown option: -$OPTARG" >&2
+      show_help
+      ;;
+    :)
+      echo "option requires an argument -$OPTARG." >&2
+      show_help
+      ;;
+  esac
+done
+
+if [ "$#" -le 0 ]
+then
+  echo "script requires an option"
+  show_help
+fi
+
+if [ -z "$image" ]
+then
+  echo "'-i' option is mandatory"
+  show_help
+fi
+
+if [ -z "$remote" ]
+then
+  echo "'-r' option is mandatory"
+  show_help
+fi
+
+if [ -z "$tag" ]
+then
+  echo "'-t' option is mandatory"
+  show_help
+fi
+
+if [ -z "$user" ]
+then
+  echo "'-u' option is mandatory"
+  show_help
+fi
+
+provide_container
